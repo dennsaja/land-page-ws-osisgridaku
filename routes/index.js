0ee47146a5d2ca5836bc62d4d0a76e5f26aeea0f
Denis <136689391+dennsaja@users.noreply.github.com>
2025-07-10 10:51:30 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ const path = require('path');
 router.get("/", async (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
+router.get("/404", async (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'notfound.html'));
+});
 
 module.exports = router;
