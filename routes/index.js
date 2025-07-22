a5cd5b115a916a555450493c1735422239024cf1
Denis <136689391+dennsaja@users.noreply.github.com>
2025-07-22 18:28:22 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ const path = require('path');
 router.get("/", async (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
+router.get("/error?id=MDICEEC93ND9DEDJDD404CICJ4I44INN4IFJ4FIJWXNCNDJDEI9994333", async (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'error-sytem.html'));
+});
 
 module.exports = router;
