af8e1d9e233f075a27516fc30dcb2589d6e50521
Denis <136689391+dennsaja@users.noreply.github.com>
2025-07-22 18:42:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import packages
 const express = require("express");
 const path = require('path');
-const err = require("./routes/error-system");
+const errorsys = require("./routes/error-system");
 const index = require("./routes/index");
 
 // Middlewares
@@ -11,7 +11,7 @@ app.use(express.json());
 // Routes
 
 app.use("/", index);
-app.use("/error?id=MDICEEC93ND9DEDJDD404CICJ4I44INN4IFJ4FIJWXNCNDJDEI9994333", err);
+app.use("/e", errorsys);
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', 'notfound.html'));
 });
